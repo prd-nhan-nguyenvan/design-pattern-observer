0aca2fa3ba7b6bc10511230dafbb68dc78928021
Nhan Nguyen Van Khanh <nhan.nguyenvan@paradox.ai>
2024-12-03 16:04:45 +0700

    add more action for subscribe

--- application.py
+++ application.py
@@ -6,11 +6,16 @@ class Application:
     def config(self):
         editor = Editor()
 
-        logger = LoggingListener(
+        open_logger = LoggingListener(
             "logs/log.txt",
             "Someone has opened the file: %s"
         )
-        editor.events.subscribe("open", logger)
+        save_logger = LoggingListener(
+            "logs/log.txt",
+            "Someone has saved the file: %s"
+        )
+        editor.events.subscribe("open", open_logger)
+        editor.events.subscribe("save", save_logger)
 
         email_alerts = EmailAlertsListener(
             "admin@example.com",
@@ -20,4 +25,7 @@ class Application:
 
         # Simulate some actions
         editor.open_file("examples/example.txt")
+        editor.save_file()
+
+        editor.open_file("examples/another_example.txt")
         editor.save_file()
